77c176591c3c1cf57f7e68e04d0bf4f68a994b5d
Aleksa Matic <aleksa.matic10@gmail.com>
2023-09-03 23:43:32 +0200

    Minor Modification to SearchView.tsx

--- src/components/SearchView.tsx
+++ src/components/SearchView.tsx
@@ -14,22 +14,24 @@ const SearchView = ({ posts, searchText, users }: SearchViewProps) => {
     }
   }, [searchText]);
 
+  const htmlCards = searchResults.map((post: PostData) => {
+    return (
+      <li className="list-group" key={post.id}>
+        <Card
+          postId={post.id}
+          title={post.title}
+          body={post.body}
+          author={users[post.userId]}
+        />
+      </li>
+    );
+  });
+
   return (
     <div className="w-75 mx-auto mb-3">
       <h1 className="display-3 p-3 mb-3 mx-auto text-center">Posts</h1>
       <ul className="list d-flex flex-wrap gap-3 justify-content-center">
-        {searchResults.map((post: PostData) => {
-          return (
-            <li className="list-group" key={post.id}>
-              <Card
-                postId={post.id}
-                title={post.title}
-                body={post.body}
-                author={users[post.userId]}
-              />
-            </li>
-          );
-        })}
+        {htmlCards}
       </ul>
     </div>
   );
